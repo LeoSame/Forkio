ef5373f4964bc093ed5516fd18e5f13db91df435
LeoSame <leo.sam@ukr.net>
2020-11-03 04:30:19 +0200

    1

--- gulp-tasks/clean.js
+++
@@ -1,7 +0,0 @@
-const del = require('del');
-
-const clean = () => {
-	return del(['dist/*', 'index.html'])
-}
-
-exports.clean = clean;
--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-const burger = document.querySelector('.header__burger');
-const headerMenu = document.querySelector('.header-menu__list');
-
-burger.addEventListener("click", dropMenu)
-
-function dropMenu() {
-    headerMenu.classList.toggle('header__menu--active');
-    burger.classList.toggle('header__menu--active');
-};
